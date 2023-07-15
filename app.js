271cddec1ede974609e3a638a54e32c2f21c3c2e
yenamalavamsi <138580676+yenamalavamsi@users.noreply.github.com>
2023-07-15 10:35:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var image=document.getElementById("image")
+function bulb_on(){
+    image.src="https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg?w=2000";
+}
+function bulb_off(){
+    image.src="https://i.pinimg.com/564x/e0/fd/25/e0fd25f9127a9a109a0648c83ee61643.jpg";
+}
